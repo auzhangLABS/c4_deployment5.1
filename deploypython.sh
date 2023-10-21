7a106d7a433a0e0259dd38d34eedb217441d3664
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-10-20 21:16:44 -0400

    Add files via upload

---
+++ deploypython.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+sudo apt update
+
+sudo apt install -y default-jre
+
+sudo apt install -y software-properties-common 
+
+sudo add-apt-repository -y ppa:deadsnakes/ppa 
+
+sudo apt install -y python3.7 
+
+sudo apt install -y python3.7-venv
+
+sudo apt update
