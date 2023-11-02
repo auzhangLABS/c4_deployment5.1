383964632ae63b8e77aefba314d780d44d19c673
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-11-01 21:16:45 -0400

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -7,10 +7,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_bcrypt import Bcrypt
 from flask import Flask
 app = Flask(__name__)
-#engine = create_engine('sqlite:///database.db',connect_args={'check_same_thread': False},echo=True)
-#Base.metadata.bind = engine
-DATABASE_URL = 'mysql+mysqldb://admin:password@mydatabase2.ccbbyndnxqmj.us-east-1.rds.amazonaws.com/d5banking?charset=utf8mb4'
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=True)
+engine = create_engine('sqlite:///database.db',connect_args={'check_same_thread': False},echo=True)
 Base.metadata.bind = engine
 db = scoped_session(sessionmaker(bind=engine))
 bcrypt = Bcrypt(app)
